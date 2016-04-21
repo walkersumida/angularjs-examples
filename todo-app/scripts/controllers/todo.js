1ca9667bb19ed368054c264d5925b8022ecff98a
WalkerSumida <walkersumida@gmail.com>
2016-04-21 10:51:28 +0900

    Add TODO add function

--- todo-app/scripts/controllers/todo.js
+++ todo-app/scripts/controllers/todo.js
@@ -1,4 +1,22 @@
 angular.module('app')
     .controller('TodoController', [function () {
         this.data = window.TodoApp.todo.data;
+        this.new = "";
+
+        this.add = function add() {
+            this.data.push({id: this._maxId() + 1, title: this.new});
+            this.new = "";
+        };
+
+        this._maxId = function _maxId() {
+            ary = this.data;
+            result = 0;
+            for (var i = 0; i < ary.length; i++) {
+                if (ary[i].id > result) {
+                    result = ary[i].id;
+                }
+            }
+
+            return result;
+        }
     }]);
